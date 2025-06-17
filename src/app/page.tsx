27ed2c095aa473dd9c2c31915facf4a4c09893d0
Andreas M Lbn Gaol <lgandre45@gmail.com>
2025-06-17 18:05:36 +0700

    Add database URL to the Home page display
    Introduced `DATABASE_URL` from environment variables and displayed it on the Home page. This helps confirm the correct database configuration during development or debugging.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+const databaseUrl = process.env.DATABASE_URL;
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -21,7 +23,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            { databaseUrl }
           </li>
         </ol>
 
